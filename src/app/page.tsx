0cbc9d42bb22060a82b0e4aed79f3b0cdcfc1d85
PavelProkopenya <prokopenya.personal@gmail.com>
2023-11-03 12:33:30 +0300

    feat: replace nextjs fonts import with vanilla css import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,41 @@
 
 import Link from "next/link";
 import Loader from "../components/Loader";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
-    <div>
-      <p>Home</p>
-      <Loader show />
-      <Link href={"./about"}>to about page</Link>
-    </div>
+    <>
+      <nav className="navbar">
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed in and has username */}
+        {true && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/username`}>
+                {/* <Image src={user?.photoURL} alt="user avatar" /> */}
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+      <main>
+        <p>Home</p>
+        <Loader show />
+        <Link href={"./about"}>to about page</Link>
+      </main>
+    </>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Navbar() {
+  const user = null;
+  const username = null;
+
+  return (
+    <nav className={`navbar ${inter.className}`}>
+      <ul>
+        <li>
+          <Link href="/">
+            <button className="btn-logo">FEED</button>
+          </Link>
+        </li>
+
+        {/* user is signed in and has username */}
+        {username && (
+          <>
+            <li className="push-left">
+              <Link href="/admin">
+                <button className="btn-blue">Write posts</button>
+              </Link>
+            </li>
+            <li>
+              <Link href={`/${username}`}>
+                <Image src={user?.photoURL} alt="user avatar" />
+              </Link>
+            </li>
+          </>
+        )}
+
+        {/* user is signed out OR not create username */}
+        {!username && (
+          <li>
+            <Link href="/enter">
+              <button className="btn-blue">Log in</button>
+            </Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
