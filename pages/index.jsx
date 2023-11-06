58d645ddc619d64e30c03340d5d8383375f1ebd2
PavelProkopenya <prokopenya.personal@gmail.com>
2023-11-06 15:23:03 +0300

    feat: add toast

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Loader from "../components/Loader";
-
+import toast from "react-hot-toast";
+ 
 export default function Home() {
   return (
     <main>
       <Loader show />
       <Link href="/testLink">link to /testLink</Link>
+      <button onClick={() => toast.success("hello toast!")}>Toast me</button>
     </main>
   );
 }
